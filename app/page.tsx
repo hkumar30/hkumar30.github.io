50bea33615508fbfbb447e5870f3e8806eaf72c5
hkumar30 <hkumar30@asu.edu>
2026-06-05 04:03:18 -0700

    adeed music component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { PageTransition } from '@/components/PageTransition';
 import HomeHero from '@/components/HomeHero';
 import HomeMonologue from '@/components/HomeMonologue';
+import MusicWidget from '@/components/MusicWidget';
 
 export default function HomePage() {
   return (
     <PageTransition className="home-shell">
       <HomeHero />
       <HomeMonologue />
+      <MusicWidget />
     </PageTransition>
   );
 }
